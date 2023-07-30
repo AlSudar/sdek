782086de01a1133cb2e3064a54f36c7d2ab42920
AlexSudarev <sudarevwork@gmail.com>
2023-07-30 19:37:18 +0300

    a

--- out/_next/static/7NKzVQbQ9FSl2g32cJomz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/648-7803ae506095d94d.js","static/css/73ed2ce99cdad046.css","static/chunks/pages/index-6ce0d0409b81bc9e.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
