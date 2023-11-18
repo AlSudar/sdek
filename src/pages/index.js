c12f6882310e00fcc410e84c128fcafe254d6a26
AlexSudarev <sudarevwork@gmail.com>
2023-11-18 13:24:17 +0300

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,42 @@
+import localFont from 'next/font/local';
+import style from './style.module.scss';
+import cn from 'classnames';
+import Image from 'next/image';
+import { useEffect } from 'react';
+
+const myFont = localFont({
+  src: [
+    {
+      path: './fonts/TTFirsNeueMedium.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/TTFirsNeueRegular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+});
+
 export default function Home() {
+  function onEntry(entry) {
+    entry.forEach((change) => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element-show');
+      }
+    });
+  }
+
+  useEffect(() => {
+    let options = { threshold: [0.1] };
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = document.querySelectorAll('.animation');
+    for (let elm of elements) {
+      observer.observe(elm);
+    }
+  }, []);
+
   return (
     <>
       {/* <Head>
@@ -49,7 +87,72 @@ export default function Home() {
           type='image/svg+xml'
         />
       </Head> */}
-      <div>lorem </div>
+      <div className={cn(myFont.className, style.wrapper)}>
+        <header className={style.header}>
+          <a target='_blank' href=''>
+            <Image
+              width={247}
+              height={23}
+              alt='Логотип Xota и Модус'
+              src='/header/modus.svg'
+              className={style.headerModus}
+              priority
+            />
+          </a>
+          <a target='_blank' href=''>
+            <Image
+              width={20}
+              height={20}
+              src='/header/closeIcon.svg'
+              className={style.headerCloseIcon}
+            />
+          </a>
+          <a target='_blank' href=''>
+            <Image
+              width={110}
+              height={20}
+              alt='Логотип Ведомости'
+              src='/header/vedomosty.svg'
+              className={style.headerVedomosty}
+              priority
+            />
+          </a>
+        </header>
+        <main className={style.main}>
+          <h1 className={style.title}>В&nbsp;поиске контакта</h1>
+          <div className={style.content}>
+            <p className={cn(style.contentLeft, 'animation')}>
+              Как лидеры российского бизнеса адаптируются к изменениям в
+              потребительском поведении и экономике
+            </p>
+            <p className={cn(style.contentRight, 'animation')}>
+              Скорость трансформационных процессов стремительно растет — сегодня
+              компаниям приходится внедрять инновации и адаптировать стратегии в
+              считанные недели. Чтобы понять, какие факторы больше всего
+              повлияли на потребление в ключевых отраслях российского бизнеса и
+              как бизнес приспосабливался к этим переменам, CRM-платформа НОТА
+              МОДУС (холдинг Т1) провела масштабное исследование. В нем приняли
+              участие более 150 директоров по маркетингу и клиентскому сервису в
+              11 отраслях экономики.
+            </p>
+          </div>
+          <p className={cn(style.description, 'animation')}>
+            Внутри: ключевые тренды и вызовы последних двух лет, решения,
+            которые принимали компании, с детализацией по отраслям и сегментам
+            B2B/B2C.
+          </p>
+        </main>
+        <footer className={style.footer}>
+          <a
+            download
+            target='_blank'
+            href='/pdf/НОТА_МОДУС_Взаимодействие_с_клиентами1.pdf'
+            className={style.footerButton}
+          >
+            Скачать исследование
+          </a>
+        </footer>
+      </div>
     </>
   );
 }
