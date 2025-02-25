87ae31073a66e588f3e18b3f1d1fdaf2bf09fbc6
AlSudar <47494628+AlSudar@users.noreply.github.com>
2025-02-25 18:04:08 +0300

    add update

--- out/_next/static/zPzYGxoGZLBPYHsCsSIMK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/749-de861dff965fd586.js","static/css/96df6cc35b246653.css","static/chunks/pages/index-1a7e1b6495d25284.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
